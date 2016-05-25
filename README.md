8434bb5449eb12dc407e751a0978f098c2b73cd6
wusfen <404315887@qq.com>
2016-05-25 11:34:42 +0800

    ...

---
+++ README.md
@@ -0,0 +1,19 @@
+# wu.form.js
+
+## USAGE 用法
+```javascript
+
+// create
+var form = wu.form({
+	url: url,
+	method: 'post',
+	data: {
+		name: 'tom',
+		password: 'pass'
+	}
+});
+
+// submit
+form.submit();
+
+```
---
+++ wu.form.js
@@ -0,0 +1,42 @@
+/**
+ * 用 js 创建表单，然后提交
+ * 
+ * 2015
+ *
+ * @author wushufen
+ *
+ * @example
+ * wu.form({
+ * 	url: url,
+ * 	method: 'post',
+ * 	data: {
+ * 		name: 'tom',
+ * 		password: 'pass'
+ * 	}
+ * }).submit();
+ */
++ function(wu) {
+	wu.form = function(options) {
+		var options = options || {};
+
+		var form = document.createElement('form');
+		form.action = options.action || options.url || '';
+		form.method = options.method || 'post';
+
+		var data = options.data;
+		for (var i in data) {
+			var field = document.createElement('input');
+			// field.type = 'hidden';
+			field.name = i;
+			field.value = data[i];
+
+			form.appendChild(field);
+		}
+
+		//ie要加到页面上才能提交
+		form.style.display = 'none';
+		document.body.appendChild(form);
+
+		return form;
+	};
+}(this.wu || (wu = {}));
